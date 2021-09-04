7ef4c71dbc7268ab780ae5d3e35d3db1c42a8a2d
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-04 17:42:30 -0300

    feat(web): create SignIn function in AuthContext

--- packages/web/src/contexts/AuthContext.tsx
+++ packages/web/src/contexts/AuthContext.tsx
@@ -1,22 +1,70 @@
-import { createContext, FC } from "react"
+import { createContext, FC, useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { setCookie, parseCookies } from "nookies"
 
-import { useFetch } from "@hooks"
+import { SignInRequest, getUserInformation } from "@services"
+
+type SignInData = {
+  name: string
+  email: string
+  password: string
+}
+
+type User = {
+  id: string
+  name: string
+  email: string
+}
 
 type AuthContextType = {
   isAuthenticated: boolean
+  user: User
+  signIn: ({}: SignInData) => Promise<void>
 }
 
 export const AuthContext = createContext({} as AuthContextType)
 
 export const Auth: FC = ({ children }) => {
-  const isAuthenticated = false
+  const [user, setUser] = useState<User | null>(null)
+  const { push } = useRouter()
+  const isAuthenticated = !!user
 
-  const signIn = async () => {
+  useEffect(() => {
+    const { "fastgas.token": token } = parseCookies()
+
+    if (token) {
+      const { name, email, id } = getUserInformation(token) as User
+      setUser({
+        name,
+        email,
+        id
+      })
+    }
+  }, [])
+
+  const signIn = async ({ name, email, password }: SignInData) => {
     /* call API */
+    const { data } = await SignInRequest({
+      name,
+      email,
+      password
+    })
+
+    const { accessToken, user } = data
+
+    setCookie(undefined, "fastgas.token", accessToken, {
+      maxAge: 60 * 60 * 24 // 24 hrs
+    })
+
+    setUser(user)
+
+    push("/")
   }
 
+  // const logIn = async ({ email, password }: SignInData) => {}
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
   )
--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -3,14 +3,15 @@ import { AppProps } from "next/app"
 import { ThemeProvider, bitcasinoDark } from "@heathmont/moon-themes"
 
 import { SEO } from "../document"
+import { Header } from "@components"
+import { Auth } from "@contexts"
 
 import "@styles/global.scss"
 import { Container } from "./styles"
-import { Header } from "@components"
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <Auth>
       <SEO />
       <ThemeProvider theme={bitcasinoDark}>
         <Container style={{ WebkitTapHighlightColor: "transparent" }}>
@@ -18,7 +19,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
           <Component {...pageProps} />
         </Container>
       </ThemeProvider>
-    </>
+    </Auth>
   )
 }
 
