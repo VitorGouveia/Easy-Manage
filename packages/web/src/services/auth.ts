07b5f3f346baa5bf28cca1f7824b827723eac35b
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-04 17:44:39 -0300

    feat(web): creates api service

--- packages/web/src/services/auth.ts
+++ packages/web/src/services/auth.ts
@@ -1,18 +1,44 @@
-import { useFetch } from "@hooks"
+import { api } from "@services"
+import { decode } from "jsonwebtoken"
 
 type RegisterRequestProps = {
   name: string
   email: string
   password: string
 }
-// type SignInRequestProps = {}
 
-export const RegisterRequest = (user: RegisterRequestProps) => {
-  const { data } = useFetch("/user", { method: "POST", data: user })
+type RegisterResponseProps = {
+  data: {
+    accessToken: string
+    message: string
+    user: {
+      email: string
+      name: string
+      id: string
+    }
+  }
+}
+
+export const SignInRequest = async ({
+  name,
+  email,
+  password
+}: RegisterRequestProps): Promise<RegisterResponseProps> => {
+  const { data }: RegisterResponseProps = await api({
+    method: "POST",
+    url: "/user",
+    data: {
+      name,
+      email,
+      password
+    }
+  })
 
   return {
     data
   }
 }
 
-// export const SignInRequest = (data: SignInRequestProps) => {}
+export const getUserInformation = (token: string) => {
+  return decode(token)
+}
