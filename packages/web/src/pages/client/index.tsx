f34b368a9204711bda7126f23e8823cbca284555
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-10 15:55:04 -0300

    fix(web): remove other pages

--- packages/web/src/pages/client/index.tsx
+++ packages/web/src/pages/client/index.tsx
@@ -1,212 +1,219 @@
-import { FC, useEffect, useState } from "react"
-import { GetServerSideProps } from "next"
-import { parseCookies } from "nookies"
-import { useForm } from "react-hook-form"
+// import { FC, useEffect, useState } from "react"
+// import { GetServerSideProps } from "next"
+// import { parseCookies } from "nookies"
+// import { useForm } from "react-hook-form"
 
-import {
-  GetClients,
-  CreateClient,
-  RemoveClient,
-  SearchClients
-} from "@services"
-import { FilesRemove, GenericEdit } from "@heathmont/moon-icons"
-import { Button, Search, TextInput } from "@heathmont/moon-components"
+// import {
+//   GetClients,
+//   CreateClient,
+//   RemoveClient,
+//   SearchClients
+// } from "@services"
+// import { FilesRemove, GenericEdit } from "@heathmont/moon-icons"
+// import { Button, Search, TextInput } from "@heathmont/moon-components"
 
-import { ClientContainer, CardContainer, Card, NewClientForm } from "./styles"
-import { useAuth } from "@hooks"
-import { Client } from "types/auth"
+// import { ClientContainer, CardContainer, Card, NewClientForm } from "./styles"
+// import { useAuth } from "@hooks"
+// import { Client } from "types/auth"
 
-type ClientProps = {
-  clients: Client[]
-}
+// type ClientProps = {
+//   clients: Client[]
+// }
 
-const ClientPage: FC<ClientProps> = ({ clients }) => {
-  const { register, handleSubmit } = useForm()
-  const [clientList, setClientList] = useState<Client[]>([])
-  const [clientSearchList, setClientSearchList] = useState<Client[]>([])
-  const { accessToken, user } = useAuth()
+// const ClientPage: FC<ClientProps> = ({ clients }) => {
+//   const { register, handleSubmit } = useForm()
+//   const [clientList, setClientList] = useState<Client[]>([])
+//   const [clientSearchList, setClientSearchList] = useState<Client[]>([])
+//   const { accessToken, user } = useAuth()
 
-  useEffect(() => setClientList(clients), [])
+//   useEffect(() => setClientList(clients), [])
 
-  const createClient = async (data: Omit<Client, "id">) => {
-    setClientList([
-      ...clientList,
-      {
-        id: String(new Date().getTime()),
-        ...data
-      }
-    ])
+//   const createClient = async (data: Omit<Client, "id">) => {
+//     setClientList([
+//       ...clientList,
+//       {
+//         id: String(new Date().getTime()),
+//         ...data
+//       }
+//     ])
 
-    await CreateClient({ ...data }, accessToken)
-  }
+//     await CreateClient({ ...data }, accessToken)
+//   }
 
-  const searchClients = async (query: string) => {
-    const clients = await SearchClients(query, accessToken)
-    setClientSearchList(clients)
-  }
+//   const searchClients = async (query: string) => {
+//     const clients = await SearchClients(query, accessToken)
+//     setClientSearchList(clients)
+//   }
 
-  const handleRemoveClient = async (clientId: string) => {
-    setClientList(clientList.filter(client => client.id !== clientId))
+//   const handleRemoveClient = async (clientId: string) => {
+//     setClientList(clientList.filter(client => client.id !== clientId))
 
-    await RemoveClient(clientId, user.id, accessToken)
-  }
+//     await RemoveClient(clientId, user.id, accessToken)
+//   }
 
+//   return (
+//     <ClientContainer>
+//       <div className="search">
+//         <Search
+//           onChange={event => searchClients(event.target.value)}
+//           results={[
+//             {
+//               title: <span>clientes</span>,
+//               items: [
+//                 <>
+//                   {clientSearchList.map(client => {
+//                     return <span>{client.name}</span>
+//                   })}
+//                 </>
+//               ]
+//             }
+//           ]}
+//           loadingMessage={
+//             <CardContainer>
+//               <h1>Todos os Clientes</h1>
+
+//               <Card full={true}>
+//                 <h5>No clients yet.</h5>
+//               </Card>
+//             </CardContainer>
+//           }
+//         />
+//       </div>
+
+//       {clientList.length === 0 ? (
+//         <CardContainer>
+//           <h1>Todos os Clientes</h1>
+
+//           <Card full={true}>
+//             <h5>No clients yet.</h5>
+//           </Card>
+//         </CardContainer>
+//       ) : (
+//         <ul>
+//           <h1>Todos os Clientes</h1>
+
+//           {clientList.map(client => {
+//             return (
+//               <Card key={client.id}>
+//                 <strong>Informações:</strong>
+//                 <p>{client.name}</p>
+//                 <p>{client.phoneNumber}</p>
+
+//                 <strong>Localização:</strong>
+//                 <p>
+//                   {client.street} - {client.city}
+//                 </p>
+//                 <p>casa nº: {client.houseNumber}</p>
+
+//                 <strong>Opcional:</strong>
+//                 <p>{client.opts}</p>
+
+//                 <Button
+//                   onClick={() => handleRemoveClient(client.id)}
+//                   data-attr="remove"
+//                 >
+//                   <FilesRemove fontSize="2rem" />
+//                 </Button>
+
+//                 <Button data-attr="edit">
+//                   <GenericEdit fontSize="2rem" />
+//                 </Button>
+//               </Card>
+//             )
+//           })}
+//         </ul>
+//       )}
+
+//       <section>
+//         <h1>Novo Cliente</h1>
+
+//         <NewClientForm onSubmit={handleSubmit(createClient)}>
+//           <div className="input-wrapper">
+//             <TextInput
+//               required
+//               type="text"
+//               label="name"
+//               placeholder="nome"
+//               {...register("name")}
+//             />
+//             <TextInput
+//               required
+//               type="tel"
+//               label="telephone"
+//               placeholder="telefone"
+//               {...register("phoneNumber")}
+//             />
+//             <TextInput
+//               required
+//               type="text"
+//               label="city"
+//               placeholder="cidade"
+//               {...register("city")}
+//             />
+//             <TextInput
+//               required
+//               type="text"
+//               label="street"
+//               placeholder="rua"
+//               {...register("street")}
+//             />
+//             <TextInput
+//               required
+//               type="text"
+//               label="houseNumber"
+//               placeholder="número da casa"
+//               {...register("houseNumber")}
+//             />
+//             <TextInput
+//               required
+//               type="text"
+//               label="opts"
+//               placeholder="referência"
+//               {...register("opts")}
+//             />
+//           </div>
+
+//           <Button fullWidth variant="tertiary">
+//             Criar Cliente
+//           </Button>
+//         </NewClientForm>
+//       </section>
+//     </ClientContainer>
+//   )
+// }
+
+// export default ClientPage
+
+// export const getServerSideProps: GetServerSideProps = async ctx => {
+//   const { "fastgas.token": token } = parseCookies(ctx)
+
+//   if (!token) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//         permanent: false
+//       }
+//     }
+//   }
+
+//   const clients = await GetClients(token)
+
+//   if (!clients) {
+//     return {
+//       notFound: true
+//     }
+//   }
+
+//   return {
+//     props: {
+//       clients
+//     }
+//   }
+// }
+export default function a() {
   return (
-    <ClientContainer>
-      <div className="search">
-        <Search
-          onChange={event => searchClients(event.target.value)}
-          results={[
-            {
-              title: <span>clientes</span>,
-              items: [
-                <>
-                  {clientSearchList.map(client => {
-                    return <span>{client.name}</span>
-                  })}
-                </>
-              ]
-            }
-          ]}
-          loadingMessage={
-            <CardContainer>
-              <h1>Todos os Clientes</h1>
-
-              <Card full={true}>
-                <h5>No clients yet.</h5>
-              </Card>
-            </CardContainer>
-          }
-        />
-      </div>
-
-      {clientList.length === 0 ? (
-        <CardContainer>
-          <h1>Todos os Clientes</h1>
-
-          <Card full={true}>
-            <h5>No clients yet.</h5>
-          </Card>
-        </CardContainer>
-      ) : (
-        <ul>
-          <h1>Todos os Clientes</h1>
-
-          {clientList.map(client => {
-            return (
-              <Card key={client.id}>
-                <strong>Informações:</strong>
-                <p>{client.name}</p>
-                <p>{client.phoneNumber}</p>
-
-                <strong>Localização:</strong>
-                <p>
-                  {client.street} - {client.city}
-                </p>
-                <p>casa nº: {client.houseNumber}</p>
-
-                <strong>Opcional:</strong>
-                <p>{client.opts}</p>
-
-                <Button
-                  onClick={() => handleRemoveClient(client.id)}
-                  data-attr="remove"
-                >
-                  <FilesRemove fontSize="2rem" />
-                </Button>
-
-                <Button data-attr="edit">
-                  <GenericEdit fontSize="2rem" />
-                </Button>
-              </Card>
-            )
-          })}
-        </ul>
-      )}
-
-      <section>
-        <h1>Novo Cliente</h1>
-
-        <NewClientForm onSubmit={handleSubmit(createClient)}>
-          <div className="input-wrapper">
-            <TextInput
-              required
-              type="text"
-              label="name"
-              placeholder="nome"
-              {...register("name")}
-            />
-            <TextInput
-              required
-              type="tel"
-              label="telephone"
-              placeholder="telefone"
-              {...register("phoneNumber")}
-            />
-            <TextInput
-              required
-              type="text"
-              label="city"
-              placeholder="cidade"
-              {...register("city")}
-            />
-            <TextInput
-              required
-              type="text"
-              label="street"
-              placeholder="rua"
-              {...register("street")}
-            />
-            <TextInput
-              required
-              type="text"
-              label="houseNumber"
-              placeholder="número da casa"
-              {...register("houseNumber")}
-            />
-            <TextInput
-              required
-              type="text"
-              label="opts"
-              placeholder="referência"
-              {...register("opts")}
-            />
-          </div>
-
-          <Button fullWidth variant="tertiary">
-            Criar Cliente
-          </Button>
-        </NewClientForm>
-      </section>
-    </ClientContainer>
+    <span>
+      <h1>dwdwa</h1>
+    </span>
   )
-}
-
-export default ClientPage
-
-export const getServerSideProps: GetServerSideProps = async ctx => {
-  const { "fastgas.token": token } = parseCookies(ctx)
-
-  if (!token) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false
-      }
-    }
-  }
-
-  const clients = await GetClients(token)
-
-  if (!clients) {
-    return {
-      notFound: true
-    }
-  }
-
-  return {
-    props: {
-      clients
-    }
-  }
 }
--- packages/web/src/pages/register/index.tsx
+++ packages/web/src/pages/register/index.tsx
@@ -1,84 +1,87 @@
-import { FC } from "react"
-import { useForm } from "react-hook-form"
+// import { FC } from "react"
+// import { useForm } from "react-hook-form"
 
-import { TextInput, Button } from "@heathmont/moon-components"
-import { ArrowsLeft } from "@heathmont/moon-icons"
-import { useAuth } from "@hooks"
-import { Link } from "@components"
+// import { TextInput, Button } from "@heathmont/moon-components"
+// import { ArrowsLeft } from "@heathmont/moon-icons"
+// import { useAuth } from "@hooks"
+// import { Link } from "@components"
 
-import { RegisterContainer } from "./styles"
+// import { RegisterContainer } from "./styles"
 
-const Register: FC = () => {
-  const { signIn } = useAuth()
-  const { register, handleSubmit } = useForm()
+// const Register: FC = () => {
+//   const { signIn } = useAuth()
+//   const { register, handleSubmit } = useForm()
 
-  type SignInRequest = {
-    name: string
-    email: string
-    password: string
-  }
+//   type SignInRequest = {
+//     name: string
+//     email: string
+//     password: string
+//   }
 
-  const handleSignIn = async ({ name, email, password }: SignInRequest) => {
-    try {
-      await signIn({
-        name,
-        email,
-        password
-      })
-    } catch (error) {
-      return (
-        <>
-          <h1>somethign went wrong</h1>
-        </>
-      )
-    }
-  }
+//   const handleSignIn = async ({ name, email, password }: SignInRequest) => {
+//     try {
+//       await signIn({
+//         name,
+//         email,
+//         password
+//       })
+//     } catch (error) {
+//       return (
+//         <>
+//           <h1>somethign went wrong</h1>
+//         </>
+//       )
+//     }
+//   }
 
-  return (
-    <RegisterContainer>
-      <header>
-        <img src="/logo.png" alt="FastGas logo" />
-        <h1>Faça seu registro</h1>
-      </header>
-      <main>
-        <form onSubmit={handleSubmit(handleSignIn)}>
-          <div className="input-wrapper">
-            <TextInput
-              required
-              type="text"
-              label="name"
-              placeholder="nome"
-              {...register("name")}
-            />
-            <TextInput
-              required
-              type="text"
-              label="email"
-              placeholder="e-mail"
-              {...register("email")}
-            />
-            <TextInput
-              required
-              type="password"
-              label="password"
-              placeholder="senha"
-              {...register("password")}
-            />
-          </div>
+//   return (
+//     <RegisterContainer>
+//       <header>
+//         <img src="/logo.png" alt="FastGas logo" />
+//         <h1>Faça seu registro</h1>
+//       </header>
+//       <main>
+//         <form onSubmit={handleSubmit(handleSignIn)}>
+//           <div className="input-wrapper">
+//             <TextInput
+//               required
+//               type="text"
+//               label="name"
+//               placeholder="nome"
+//               {...register("name")}
+//             />
+//             <TextInput
+//               required
+//               type="text"
+//               label="email"
+//               placeholder="e-mail"
+//               {...register("email")}
+//             />
+//             <TextInput
+//               required
+//               type="password"
+//               label="password"
+//               placeholder="senha"
+//               {...register("password")}
+//             />
+//           </div>
 
-          <Button fullWidth variant="primary">
-            Criar conta
-          </Button>
-        </form>
-      </main>
-      <footer>
-        <Link name="go to login" url="/login">
-          <ArrowsLeft color="white" fontSize="1rem" />
-          <small>Já tenho conta?</small>
-        </Link>
-      </footer>
-    </RegisterContainer>
-  )
+//           <Button fullWidth variant="primary">
+//             Criar conta
+//           </Button>
+//         </form>
+//       </main>
+//       <footer>
+//         <Link name="go to login" url="/login">
+//           <ArrowsLeft color="white" fontSize="1rem" />
+//           <small>Já tenho conta?</small>
+//         </Link>
+//       </footer>
+//     </RegisterContainer>
+//   )
+// }
+
+// export default Register
+export default function b() {
+  return <h1>a</h1>
 }
-
-export default Register
